bf3f4ef80968093b0110762bf5284f00ea6d2a3a
VictorBaertPXL <victor.baert@student.pxl.be>
2025-05-27 11:13:54 +0200

    Final checks done

--- Dungeon.c
+++ Dungeon.c
@@ -6,33 +6,32 @@
 #define MAX_ROOMS 100
 #define MAX_DIRECTION 4
 
-// structs en enums aanmaken
+// Enums
 typedef enum 
-{
+{ 
     EMPTY,
-    MONSTER,
-    ITEM,
-    TREASURE
+    MONSTER, 
+    ITEM, 
+    TREASURE 
 } roomType;
 
 typedef enum 
-{
-    GOBLIN,
-    TROLL
+{ 
+    GOBLIN, 
+    TROLL 
 } monsterType;
-
-typedef enum
-{
+typedef enum 
+{ 
     DAMAGE,
-    HEALTH
+    HEALTH 
 } itemType;
 
+// Structs
 typedef struct 
 {
     monsterType type;
     int hp;
     int damage;
-
 } monster_struct;
 
 typedef struct 
@@ -41,7 +40,20 @@ typedef struct
     int value;
 } item_struct;
 
-typedef struct room_struct
+typedef struct player_struct
+{
+    int hp;
+    int damage;
+    int room_id;
+} player_struct;
+
+// Forward declaration
+struct room_struct;
+
+// Function pointer type
+typedef void (*room_action)(player_struct*, struct room_struct*);
+
+typedef struct room_struct 
 {
     int id;
     roomType type;
@@ -49,20 +61,17 @@ typedef struct room_struct
     struct room_struct* directions[MAX_DIRECTION];
     monster_struct* monster;
     item_struct* item;
+    room_action action; // <<< Function pointer
 } room_struct;
 
-typedef struct
-{
-    int hp;
-    int damage;
-    int room_id;
-} player_struct;
-
+// Globals
 room_struct* rooms[MAX_ROOMS];
 int room_count = 0;
 int treasure_placed = 0;
 int total_rooms = 0;
 
+
+// Function declarations
 room_struct* create_room(int id);
 void connect_rooms(room_struct* a, room_struct* b);
 void generate_dungeon(int num_rooms);
@@ -73,45 +82,86 @@ void print_room_info(room_struct* room);
 void save_game(player_struct* player, const char* filename);
 int load_game(player_struct* player, const char* filename);
 
-// aanmaken van de kamers
-room_struct* create_room(int id)
+
+// Room actions
+void action_empty(player_struct* player, room_struct* room) 
+{
+    printf("The room is empty.\n");
+}
+
+void action_monster(player_struct* player, room_struct* room) 
+{
+    fight(player, room->monster);
+    free(room -> monster);
+    room -> monster = NULL;
+    room -> type = EMPTY;
+    room -> action = action_empty;
+}
+
+void action_item(player_struct* player, room_struct* room) 
+{
+    if (room -> item -> type == HEALTH) 
+    {
+        printf("You found a health potion! (+%d hp)\n", room -> item -> value);
+        player -> hp += room->item->value;
+    } 
+    else 
+    {
+        printf("You found a damage potion! (+%d dmg)\n", room->item->value);
+        player->damage += room->item->value;
+    }
+
+    free(room->item);
+    room -> item = NULL;
+    room -> type = EMPTY;
+    room -> action = action_empty;
+}
+
+void action_treasure(player_struct* player, room_struct* room) 
+{
+    printf("You found the treasure!!! Good job!\n");
+}
+
+// Kamers aanmaken
+room_struct* create_room(int id) 
 {
     room_struct* room = malloc(sizeof(room_struct));
-    room->id = id;
-    room->visited = 0;
-    room->monster = NULL;
-    room->item = NULL;
-
+    room -> id = id;
+    room -> visited = 0;
+    room -> monster = NULL;
+    room -> item = NULL;
     for (int i = 0; i < MAX_DIRECTION; i++) room->directions[i] = NULL;
 
-    // Als de schat nog niet geplaatst is, en dit is niet kamer 0,
-    // dan plaats je met 30% kans een schat, OF verplicht in de laatste kamer
     if (!treasure_placed && id != 0 && ((rand() % 100) < 30 || id == total_rooms - 1)) 
     {
-        room->type = TREASURE;
+        room -> type = TREASURE;
         treasure_placed = 1;
-    }
-    else
+        room -> action = action_treasure;
+    } 
+    else 
     {
         int chance = rand() % 100;
-        if (chance < 30)
+        if (chance < 30) 
         {
-            room-> type = MONSTER;
-            room-> monster = malloc(sizeof(monster_struct));
-            room-> monster-> type = rand() % 2;
-            room-> monster-> hp = 5 + rand() % 10;
-            room-> monster-> damage = 2 + rand() % 3;
-        }
-        else if (chance < 60)
+            room -> type = MONSTER;
+            room -> monster = malloc(sizeof(monster_struct));
+            room -> monster -> type = rand() % 2;
+            room -> monster -> hp = 5 + rand() % 10;
+            room -> monster -> damage = 2 + rand() % 3;
+            room -> action = action_monster;
+        } 
+        else if (chance < 60) 
         {
-            room-> type = ITEM;
-            room-> item = malloc(sizeof(item_struct));
-            room-> item-> type = rand() % 2;
-            room-> item-> value = 3 + rand() % 3;
-        }
-        else
+            room -> type = ITEM;
+            room -> item = malloc(sizeof(item_struct));
+            room -> item->type = rand() % 2;
+            room -> item->value = 3 + rand() % 3;
+            room -> action = action_item;
+        } 
+        else 
         {
-            room->type = EMPTY;
+            room -> type = EMPTY;
+            room -> action = action_empty;
         }
     }
 
@@ -119,165 +169,120 @@ room_struct* create_room(int id)
     return room;
 }
 
-// kamers aan elkaar verbinden
-void connect_rooms(room_struct* a, room_struct* b)
+void connect_rooms(room_struct* a, room_struct* b) 
 {
-    for (int i = 0; i < MAX_DIRECTION; i++)
+    for (int i = 0; i < MAX_DIRECTION; i++) 
     {
-        if (a->directions[i] == NULL)
+        if (a -> directions[i] == NULL) 
         {
-            a->directions[i] = b;
+            a -> directions[i] = b;
             break;
         }
     }
-
-    for (int i = 0; i < MAX_DIRECTION; i++)
+    for (int i = 0; i < MAX_DIRECTION; i++) 
     {
-        if (b->directions[i] == NULL)
+        if (b -> directions[i] == NULL) 
         {
-            b->directions[i] = a;
+            b -> directions[i] = a;
             break;
         }
     }
 }
 
-// Dungeon genereren
-void generate_dungeon(int num_rooms)
+void generate_dungeon(int num_rooms) 
 {
-    for (int i = 0; i < num_rooms; i++)
-    {
-        create_room(i);
-    }
-    for (int i = 1; i < num_rooms; i++)
+    for (int i = 0; i < num_rooms; i++) create_room(i);
+    for (int i = 1; i < num_rooms; i++) 
     {
         int connect = rand() % i;
         connect_rooms(rooms[i], rooms[connect]);
     }
 }
 
-// betreden van een kamer
-void enter_room(player_struct* player, room_struct* room)
+void enter_room(player_struct* player, room_struct* room) 
 {
     printf("You have entered room %d\n", room->id);
-    printf("Current hp: %d, dmg: %d\n", player->hp, player -> damage);
+    printf("Current hp: %d, dmg: %d\n", player->hp, player->damage);
 
-    if (room->visited == 1) 
+    if (!room -> visited) 
     {
-        return;
-    }
-    else
-    {
-        room->visited = 1;
-    }
-
-    if (room->type == MONSTER && room->monster)
-    {
-        fight(player, room->monster);
-        free(room->monster);
-        room->monster = NULL;
-        room->type = EMPTY;
-    }
-    else if (room->type == ITEM && room->item)
-    {
-        if (room->item->type == HEALTH)
-        {
-            printf("You found a health potion! (+%d hp)\n", room->item->value);
-            player->hp += room->item->value;
-        }
-        else
-        {
-            printf("You found a damage potion! (+%d dmg)\n", room->item->value);
-            player->damage += room->item->value;
-        }
-        free(room->item);
-        room->item = NULL;
-        room->type = EMPTY;
+        room -> visited = 1;
+        room -> action(player, room); // <<< execute function pointer
     }
 }
 
-// vechten
-void fight(player_struct* player, monster_struct* monster)
+void fight(player_struct* player, monster_struct* monster) 
 {
-    printf("A monster has appeared!!! (%d hp, %d dmg)\n", monster->hp, monster->damage);
-
-    while (player->hp > 0 && monster->hp > 0)
+    printf("A monster has appeared!!! (%d hp, %d dmg)\n", monster -> hp, monster -> damage);
+    while (player -> hp > 0 && monster -> hp > 0) 
     {
         int bits = rand() % 32;
         printf("Attack pattern...");
-
-        for (int i = 4; i >= 0; i--)
+        for (int i = 4; i >= 0; i--) 
         {
             int mask = 1 << i;
-            if (bits & mask)
+            if (bits & mask) 
             {
-                monster->hp -= player->damage;
+                monster -> hp -= player -> damage;
                 printf("1");
-            }
-            else
+            } 
+            else 
             {
-                player->hp -= monster->damage;
+                player -> hp -= monster -> damage;
                 printf("0");
             }
         }
-
-        printf("\nPlayer HP: %d, Monster HP: %d\n", player->hp, monster->hp);
+        printf("\nPlayer HP: %d, Monster HP: %d\n", player -> hp, monster -> hp);
     }
 }
 
-// printen van de rooms 
-void print_room_info(room_struct* room)
+void print_room_info(room_struct* room) 
 {
     printf("The room has doors leading to: ");
-    for (int i = 0; i < MAX_DIRECTION; i++)
+    for (int i = 0; i < MAX_DIRECTION; i++) 
     {
-        if (room->directions[i])
+        if (room->directions[i]) 
         {
-             printf("%d ", room->directions[i]->id);
+            printf("%d ", room->directions[i]->id);
         }
     }
     printf("\n");
 }
 
-// opslaan van de game
-void save_game(player_struct* player, const char* filename)
+void save_game(player_struct* player, const char* filename) 
 {
     FILE* f = fopen(filename, "w");
-    if(!f)
-    {
-        return;
-    }
-    fprintf(f, "%d %d %d %d\n", player->room_id, player->hp, player->damage, total_rooms);
+    if (!f) return;
+    fprintf(f, "%d %d %d %d\n", player -> room_id, player -> hp, player -> damage, total_rooms);
     fclose(f);
 }
 
-// Laden van de game
 int load_game(player_struct* player, const char* filename) 
 {
     FILE* f = fopen(filename, "r");
     if (!f) return 0;
     int rooms;
-    fscanf(f, "%d %d %d %d", &player->room_id, &player->hp, &player->damage, &rooms);
+    fscanf(f, "%d %d %d %d", &player -> room_id, &player -> hp, &player -> damage, &rooms);
     fclose(f);
     total_rooms = rooms;
     generate_dungeon(total_rooms);
     return 1;
 }
 
-// Geheugen vrijmaken
 void free_dungeon() 
 {
     for (int i = 0; i < room_count; i++) 
     {
-        if (rooms[i]->monster) free(rooms[i]->monster);
-        if (rooms[i]->item) free(rooms[i]->item);
+        if (rooms[i] -> monster) free(rooms[i] -> monster);
+        if (rooms[i] -> item) free(rooms[i] -> item);
         free(rooms[i]);
     }
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) 
 {
     srand(time(NULL));
-    player_struct player = {100,15,0};
+    player_struct player = {100, 15, 0};
 
     if (argc < 2) 
     {
@@ -296,8 +301,7 @@ int main(int argc, char* argv[])
     else 
     {
         int numb_rooms = atoi(argv[1]);
-
-        total_rooms = numb_rooms; 
+        total_rooms = numb_rooms;
         generate_dungeon(numb_rooms);
         player.room_id = 0;
     }
@@ -312,23 +316,20 @@ int main(int argc, char* argv[])
             printf("The player has died.\n");
             break;
         }
-        if (current->type == TREASURE) 
+        if (current -> type == TREASURE) 
         {
-            printf("You found the treasure!!! Good job!\n");
             break;
         }
 
         print_room_info(current);
-
         printf("Choose room id: ");
         int choice;
         scanf("%d", &choice);
 
-        // Controleer of gekozen kamer direct verbonden is met huidige kamer
         int valid_choice = 0;
         for (int i = 0; i < MAX_DIRECTION; i++) 
         {
-            if (current->directions[i] && current->directions[i]->id == choice) 
+            if (current -> directions[i] && current -> directions[i] -> id == choice) 
             {
                 valid_choice = 1;
                 break;
@@ -338,7 +339,6 @@ int main(int argc, char* argv[])
         if (valid_choice) 
         {
             player.room_id = choice;
-
             printf("Do you want to save? (y/n): ");
             char save;
             scanf(" %c", &save);
@@ -347,6 +347,7 @@ int main(int argc, char* argv[])
             {
                 save_game(&player, "savegame.txt");
             }
+
         } 
         else 
         {
